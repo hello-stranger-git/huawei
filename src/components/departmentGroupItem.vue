d25f489954b6b043c44545e507e06b22e8ccccef
xyc <1365884247@qq.com>
2021-01-26 14:59:53 +0800

    添加通讯录选择人员公共组件

---
+++ src/components/departmentGroupItem.vue
@@ -0,0 +1,68 @@
+<!--单个部门分组-->
+<template>
+  <div class="departmentGroupItem">
+    <div class="left">
+      <img :src="leftIcon"  v-if="leftIcon"/>
+    </div>
+    <div :class="[borderBoolean?'borderBottom':'','right']">
+      <span>{{ title }}</span>
+      <img :src="rightIcon" v-if="rightIcon"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    leftIcon: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    rightIcon: {
+      type: String,
+      default: ''
+    },
+    borderBoolean: {
+      type: Boolean,
+      default: true
+    }
+  },
+  mounted() {
+    console.log(this.borderBoolean)
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.departmentGroupItem{
+  padding:14px;
+  background-color: #fff;
+  display: flex;
+}
+.left{
+  img{
+    width:30px;
+    height:30px
+  }
+}
+.right{
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding-top: 4px;
+  margin-left: 10px;
+  img{
+    vertical-align: middle;
+    width:24px;
+    height:24px
+  }
+}
+.borderBottom{
+  border-bottom:1px solid rgba(112, 112, 112, 0.3);
+  padding-bottom: 18px;
+}
+</style>
--- src/views/phoneConnect/index.vue
+++ src/views/phoneConnect/index.vue
@@ -2,14 +2,29 @@
 <template>
   <div>
     <TopTitle title="通讯录" :leftArrowHidden="false"/>
+    <DepartmentGroupItem v-for="item in DepartmentGroupData" :key="item.id" :leftIcon="item.leftIcon" :title="item.title" :rightIcon="item.rightIcon" :borderBoolean="true"/>
   </div>
 </template>
 
 <script>
 import TopTitle from '@/components/topTitle'
+import DepartmentGroupItem from '@/components/departmentGroupItem'// 每一行
 export default {
+  data() {
+    return {
+      DepartmentGroupData: [
+        {
+          id: 1,
+          leftIcon: require('@/assets/images/phoneConnect/itemIcon.png'),
+          rightIcon: require('@/assets/images/phoneConnect/messageIcon.png'),
+          title: '周边部门(1)'
+        }
+      ]
+    }
+  },
   components: {
-    TopTitle
+    TopTitle,
+    DepartmentGroupItem
   }
 }
 </script>
