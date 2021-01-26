d6aaae7c8aa633c20f394555cc2b7bdac70ca1be
xyc <1365884247@qq.com>
2021-01-26 16:48:31 +0800

    添加客户管理模板

---
+++ src/views/home/customerMannager/index.vue
@@ -0,0 +1,26 @@
+<!--客户管理-->
+<template>
+  <div>
+    <TopTitle title="客户管理" @onClickLeft="onClickLeft" />
+客户管理
+  </div>
+</template>
+
+<script>
+import TopTitle from '@/components/topTitle'
+export default {
+
+  components: {
+    TopTitle
+  },
+  methods: {
+    onClickLeft() {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
