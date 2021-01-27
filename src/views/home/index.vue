86388177ee6b2f165d20e24a521f9b136319fa0f
LZ <2706253331@qq.com>
2021-01-27 16:12:32 +0800

    完成首页菜单栏请假申请模块

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -110,7 +110,24 @@ export default {
           id: 7,
           title: '请假申请',
           info: 0,
-          icon: require('@/assets/images/home/vacationApplicationIcon.png')
+          icon: require('@/assets/images/home/vacationApplicationIcon.png'),
+          name: 'InsideManager', // 当前要跳转的路由
+          from: 'Home', // 当前路由
+          to: '', // 点击加号要跳转的路由
+          sonName: [
+            {
+              id: 1, // 此id与insideManager组件的id一致
+              name: ''
+            },
+            {
+              id: 2,
+              name: ''
+            },
+            {
+              id: 3,
+              name: ''
+            }
+          ]
         },
         {
           id: 8,
