388cf570b92d2f40a7e28ea61375f7065e891c3f
LZ <2706253331@qq.com>
2021-01-27 10:23:39 +0800

    完成任务管理页面

--- src/components/bigIconItem.vue
+++ src/components/bigIconItem.vue
@@ -1,16 +1,71 @@
 <!--大图标每一项-->
 <template>
-  <div>
-    大图标每一项
+  <div class="bigIconItem">
+    <div>
+      <img :src="icon" />
+      <span>{{ title }}</span>
+      <div v-if="count">{{ count }}</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    icon: {
+      type: String
+    },
+    title: {
+      type: String
+    },
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      // icon: require('@/assets/images/home/taskManager/noFinishIcon.png')
+    }
+  }
 }
 </script>
-
-<style lang="scss" scoped>
-
+<style lang="less" scoped>
+.bigIconItem {
+  position: relative;
+  width: 48%;
+  height: 76px;
+  box-shadow: 0px 0px 6px rgba(20, 20, 20, 0.1);
+  display: flex;
+  align-items: center;
+  & > div {
+    width: 142px;
+    margin: 0 auto;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    div {
+      position: absolute;
+      right: 6px;
+      top: 6px;
+      font-size: 10px;
+      color: #fff;
+      background-color: #e30011;
+      border-radius: 9px;
+      padding: 2px 5px;
+      border: 1px solid red;
+    }
+  }
+  img {
+    vertical-align: middle;
+    width: 44px;
+    height: 44px;
+  }
+  span {
+    margin-left: 20px;
+    font-size: 14px;
+    color: #141414;
+    vertical-align: middle;
+  }
+}
 </style>
--- src/views/home/taskManager/index.vue
+++ src/views/home/taskManager/index.vue
@@ -1,11 +1,27 @@
 <!--任务管理-->
 <template>
-  <div>
+  <div class="taskManager">
     <!-- 头部区域 -->
-    <TopTitle title="任务管理" @onClickLeft="onClickLeft" @onClickRight="onClickRight">
-      <img :src="addIcon" width="24px" height="24px">
+    <TopTitle
+      title="任务管理"
+      @onClickLeft="onClickLeft"
+      @onClickRight="onClickRight"
+    >
+      <img :src="addIcon" width="24px" height="24px" />
     </TopTitle>
-    <BigIconItem />
+    <!-- 任务区域 -->
+    <div class="task" v-for="item in taskData" :key="item.id">
+      <h3>{{ item.title }}</h3>
+      <div>
+        <BigIconItem
+          v-for="(item1, i) in item.process"
+          :key="i"
+          :icon="item1.icon"
+          :title="item1.text"
+          :count="item1.count"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,19 +35,66 @@ export default {
   },
   data() {
     return {
-      addIcon: require('@/assets/images/home/workReport/addIcon.png')
+      addIcon: require('@/assets/images/home/workReport/addIcon.png'),
+      // 任务数据
+      taskData: [
+        {
+          id: 1,
+          title: '我提交的',
+          process: [
+            {
+              icon: require('@/assets/images/home/taskManager/noFinishIcon.png'),
+              text: '未完成',
+              count: 9
+            },
+            {
+              icon: require('@/assets/images/home/taskManager/finishIcon.png'),
+              text: '完成'
+            }
+          ]
+        },
+        {
+          id: 2,
+          title: '分配给我的',
+          process: [
+            {
+              icon: require('@/assets/images/home/taskManager/noFinishIcon.png'),
+              text: '未完成',
+              count: 9
+            },
+            {
+              icon: require('@/assets/images/home/taskManager/finishIcon.png'),
+              text: '完成'
+            }
+          ]
+        }
+      ]
     }
   },
   methods: {
     onClickLeft() {
       this.$router.go(-1)
     },
-    onClickRight() {
-    }
+    onClickRight() {}
   }
 }
 </script>
-
-<style lang="scss" scoped>
-
+<style lang="less" scoped>
+.taskManager {
+  height: 100%;
+  background: #fff;
+}
+.task {
+  margin: 14px 12px 26px;
+  h3 {
+    font-size: 16px;
+    color: #141414;
+    margin-bottom: 16px;
+  }
+  div {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+}
 </style>
